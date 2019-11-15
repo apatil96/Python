76882888383346514b34813484cd2f63188305f9
apatil96 <48262248+apatil96@users.noreply.github.com>
2019-11-15 14:16:42 -0500

    Add files via upload

---
+++ EX8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("*              *")
+print("*            *")
+print("*          *")
+print("*        *")
+print("*     *")
+print("*  *")
+print("**")
+print("*  *")
+print("*     *")
+print("*       *")
+print("*         *")
+print("*           *")
+print("*              *")
+
+
+# In[ ]:
+
+
+
+
